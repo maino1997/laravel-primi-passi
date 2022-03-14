c9e9fc6ca3af4a2a82348eb5b27fa585cf7d448a
Sasha Mainardi <sashamainardi4@gmail.com>
2022-03-14 16:05:07 +0100

    home in view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $user = [
+        'nome' => 'Sasha',
+        'cognome' => 'Mainardi'
+    ];
+
+    return view('home', $user);
 });
